1125b9030c58f2f71f0daa6668b3b51af45d4efb
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-01-27 17:17:16 -0600

    Routed-Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { AuthLayout } from './layouts/AuthLayout';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { ForgetPassword } from './pages/ForgetPassword';
+import { NewPassword } from './pages/NewPassword';
+import { ConfirmAccount } from './pages/ConfirmAccount';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout/>}>
+          <Route index element={<Login/>}  />
+          <Route path='register' element={<Register/>}  />
+          <Route path='forget-password' element={<ForgetPassword/>}  />
+          <Route path='forget-password/:token' element={<NewPassword/>}  />
+          <Route path='confirm/:id' element={<ConfirmAccount/>}  />
+        
+        
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ frontend/src/layouts/AuthLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router'
+
+import React from 'react'
+
+export const AuthLayout = () => {
+  return (
+    <>
+        <h1>AuthLayout</h1>
+
+
+        <Outlet/>
+    </>
+  )
+}
